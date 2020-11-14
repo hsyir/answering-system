8241307d53abd6e918d908686758ecd243b6ec82
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2020-11-15 01:01:00 +0330

    Update HsyAnsweringSystemServiceProvider.php

--- src/HsyAnsweringSystemServiceProvider.php
+++ src/HsyAnsweringSystemServiceProvider.php
@@ -10,6 +10,9 @@ class HsyAnsweringSystemServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('migrations')
+        ], 'migrations');
     }
 
     public function register()
