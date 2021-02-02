3ccd408667d7129eae37c3d4ad430057f162516c
Hossein <hosseinyaghmaee@gmail.com>
2021-02-02 14:14:35 +0330

    Update CityController.php

--- src/Http/Controllers/CityController.php
+++ src/Http/Controllers/CityController.php
@@ -3,6 +3,7 @@
 namespace Hsy\AnsweringSystem\Http\Controllers;
 
 use Hsy\AnsweringSystem\Http\Resources\CityCollection;
+use Hsy\AnsweringSystem\Http\Resources\DepartmentCollection;
 use Hsy\AnsweringSystem\Models\City;
 use Illuminate\Http\Request;
 
@@ -69,54 +70,6 @@ class CityController extends Controller
     {
         return view("answering::cities.edit", compact("department"));
     }
-
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Models\Hsy\Answering\Models\City $department
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(City $department)
-    {
-        //
-    }
-
-    public function getDepartments()
-    {
-        return new DepartmentCollection(City::all());
-    }
-
-
-    public function addUser(Request $request, City $department)
-    {
-        $department->users()->attach([$request->user_id]);
-        return self::redirectBackWithSuccess("حله داداش");
-    }
-
-    public function removeUser(Request $request, City $department)
-    {
-        $department->users()->detach($request->user_id);
-        return self::redirectBackWithSuccess("حله داداش");
-    }
-
-
-    public function addOffice(City $city,Request $request)
-    {
-
-        $this->validate($request,[
-            "id"=>"required|integer|unique:offices,id",
-            "name"=>"required|unique:offices,name",
-        ]);
-        $city->offices()->create([
-            "id"=>$request->id,
-            "name"=>$request->name,
-        ]);
-
-
-        return self::redirectBackWithSuccess("اداره جدید ایجاد شد");
-    }
-
 
 
 
